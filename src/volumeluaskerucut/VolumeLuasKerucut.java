cd61d47629e0e217bb2659d9284f35ea773e942d
Vendeta1881 <daniel.felix.n@student.undip.ac.id>
2018-04-26 11:53:39 +0700

    Tugas Modul 5

---
+++ src/volumeluaskerucut/VolumeLuasKerucut.java
@@ -0,0 +1,17 @@
+
+package volumeluaskerucut;
+import java.util.Scanner;
+public class VolumeLuasKerucut {
+
+   
+    public static void main(String[] args) {
+      input kerucut = new input();
+      kerucut.volumekerucut();
+      kerucut.luaskerucut();
+      kerucut.display();
+        
+    }
+
+            }
+    
+
